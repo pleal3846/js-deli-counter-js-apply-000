d501b98fa5fe4275899c9f1d6ae2aba5b09edd0a
patrick leal <pjleal3846@icloud.com>
2019-12-23 04:06:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0)
+  
+ return "There is nobody waiting to be served!" 
+ 
+ else(katzDeliLine.length !== 0)
+ const first = katzDeliLine.shift()
+
+ return "Currently serving " + first + "."
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0)
+  
+ return "The line is currently empty."
+ 
+ else(katzDeliLine.length !== 0)
+  var line = []
+    var len = katzDeliLine.length
+    var text= "";
+    for (let i = 0; i < len; i++)
+    {text += katzDeliLine[i] + "<br>";}
+  line.push(katzDeliLine)
+  
+  return "The line is currently:" + line
+}
